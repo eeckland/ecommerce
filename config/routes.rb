b59a094434a669553c981afbe8ae241c4bfee49a
Emily Eckland <eecklan.com>
2019-08-06 09:10:08 -0500

    Added Storefront with modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'storefront/all_items'
+  get 'storefront/items_by_category'
+  get 'storefront/items_by_brand'
   devise_for :users
   resources :products #created by scaffolding
-  root "products#index" #root= whenever I type localhost:3000, the page I load up is products index
+  root 'storefront#all_items' #root= whenever I type localhost:3000, the page I load up is products index
   # it's the products controller I need to look up, and index tells me how to execute
   #(go to controller and look at def index @products = Product.all (<--.all is method inside of active record (.all is the action method)))
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
